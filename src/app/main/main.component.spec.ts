330e6aeba5d724776f1ed21f41b0abcc4df25cf8
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-28 15:36:40 +0200

    Renders dynamic user list

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -27,9 +27,25 @@ describe('MainComponent', () => {
   it('should render user list with 3 elements', async(() => {
     const users = ["Foo", "Bar", "Baz"];
 
+    component.setUsers(users);
+    fixture.detectChanges();
+
     const debugElements = fixture.debugElement.queryAll(By.css('#user-list li'));
     expect(debugElements[0].nativeElement.textContent).toContain(users[0]);
     expect(debugElements[1].nativeElement.textContent).toContain(users[1]);
     expect(debugElements[2].nativeElement.textContent).toContain(users[2]);
+  }));
+
+  it('should render user list with 4 elements', async(() => {
+    const users = ["Baz", "Foo", "Bar", "Fizz"];
+
+    component.setUsers(users);
+    fixture.detectChanges();
+
+    const debugElements = fixture.debugElement.queryAll(By.css('#user-list li'));
+    expect(debugElements[0].nativeElement.textContent).toContain(users[0]);
+    expect(debugElements[1].nativeElement.textContent).toContain(users[1]);
+    expect(debugElements[2].nativeElement.textContent).toContain(users[2]);
+    expect(debugElements[3].nativeElement.textContent).toContain(users[3]);
   }));
 });
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+const USERS = [
+  "David",
+  "Chris",
+];
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  users: string[] = USERS;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setUsers(users: string[]) {
+    this.users = users;
+  }
 }
