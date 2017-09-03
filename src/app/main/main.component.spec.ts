22cede8060c073a884cd728e59f6c6ad00fceac5
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-09-03 11:05:01 +0200

    Add section for rendering tests in main component spec

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -56,54 +56,6 @@ describe('MainComponent', () => {
 
   }));
 
-  it('should render user table with 3 elements', async(() => {
-    const testUsers = [
-      {id: 1, username: 'Foo'},
-      {id: 2, username: 'Bar'},
-      {id: 3, username: 'Baz'}
-    ];
-
-    mainComponent.users = testUsers;
-
-    mainFixture.detectChanges();
-
-    const debugElements = mainFixture.debugElement.queryAll(By.css('tbody tr'));
-    expect(debugElements[0].nativeElement.textContent).toContain(testUsers[0].username);
-    expect(debugElements[1].nativeElement.textContent).toContain(testUsers[1].username);
-    expect(debugElements[2].nativeElement.textContent).toContain(testUsers[2].username);
-  }));
-
-  it('should render user table with 4 elements', async(() => {
-    const testUsers: User[] = [
-      {id: 1, username: 'Baz'},
-      {id: 2, username: 'Foo'},
-      {id: 3, username: 'Bar'},
-      {id: 4, username: 'Fizz'}
-    ];
-
-    mainComponent.users = testUsers;
-    mainFixture.detectChanges();
-
-    const debugElements = mainFixture.debugElement.queryAll(By.css('tbody tr'));
-    expect(debugElements[0].nativeElement.textContent).toContain(testUsers[0].username);
-    expect(debugElements[1].nativeElement.textContent).toContain(testUsers[1].username);
-    expect(debugElements[2].nativeElement.textContent).toContain(testUsers[2].username);
-    expect(debugElements[3].nativeElement.textContent).toContain(testUsers[3].username);
-  }));
-
-  it('should render user details', async(() => {
-    mainComponent.users = [
-      {id: 1, name: 'name', username: 'username', email: 'email', phone: 'phone'},
-    ];
-
-    mainFixture.detectChanges();
-    const debugElements = mainFixture.debugElement.queryAll(By.css('tbody tr td'));
-
-    expect(debugElements[0].nativeElement.textContent).toContain('name');
-    expect(debugElements[1].nativeElement.textContent).toContain('username');
-    expect(debugElements[2].nativeElement.textContent).toContain('email');
-    expect(debugElements[3].nativeElement.textContent).toContain('phone');
-  }));
 
   it('should set selectedUser upon row click', async(() => {
     const selectedTestUser = {id: 0, name: 'Foo Bar', username: 'foobar', email: 'foobar@gmail.com', phone: '1234'};
@@ -119,4 +71,55 @@ describe('MainComponent', () => {
 
     expect(mainComponent.selectedUser).toBe(selectedTestUser);
   }));
+
+  describe('should render', () => {
+    it('user table with 3 elements', async(() => {
+      const testUsers = [
+        {id: 1, username: 'Foo'},
+        {id: 2, username: 'Bar'},
+        {id: 3, username: 'Baz'}
+      ];
+
+      mainComponent.users = testUsers;
+
+      mainFixture.detectChanges();
+
+      const debugElements = mainFixture.debugElement.queryAll(By.css('tbody tr'));
+      expect(debugElements[0].nativeElement.textContent).toContain(testUsers[0].username);
+      expect(debugElements[1].nativeElement.textContent).toContain(testUsers[1].username);
+      expect(debugElements[2].nativeElement.textContent).toContain(testUsers[2].username);
+    }));
+
+    it('user table with 4 elements', async(() => {
+      const testUsers: User[] = [
+        {id: 1, username: 'Baz'},
+        {id: 2, username: 'Foo'},
+        {id: 3, username: 'Bar'},
+        {id: 4, username: 'Fizz'}
+      ];
+
+      mainComponent.users = testUsers;
+      mainFixture.detectChanges();
+
+      const debugElements = mainFixture.debugElement.queryAll(By.css('tbody tr'));
+      expect(debugElements[0].nativeElement.textContent).toContain(testUsers[0].username);
+      expect(debugElements[1].nativeElement.textContent).toContain(testUsers[1].username);
+      expect(debugElements[2].nativeElement.textContent).toContain(testUsers[2].username);
+      expect(debugElements[3].nativeElement.textContent).toContain(testUsers[3].username);
+    }));
+
+    it('user details', async(() => {
+      mainComponent.users = [
+        {id: 1, name: 'name', username: 'username', email: 'email', phone: 'phone'},
+      ];
+
+      mainFixture.detectChanges();
+      const debugElements = mainFixture.debugElement.queryAll(By.css('tbody tr td'));
+
+      expect(debugElements[0].nativeElement.textContent).toContain('name');
+      expect(debugElements[1].nativeElement.textContent).toContain('username');
+      expect(debugElements[2].nativeElement.textContent).toContain('email');
+      expect(debugElements[3].nativeElement.textContent).toContain('phone');
+    }));
+  });
 });
