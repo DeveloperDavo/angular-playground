007543ca1a7d26d935b0e5539709659263088b62
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-09-07 12:47:34 +0200

    User service gets users from resolved promise

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -16,4 +16,22 @@ describe('UserService', () => {
     expect(http.get).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/users');
   });
 
+  it('should get users from resolved promise', done => {
+    const http = createSpyObj('httpClient', ['get']);
+    const userService = new UserService(http);
+
+    const testUsers = [
+      {id: 1, username: 'Foo'},
+      {id: 2, username: 'Bar'},
+      {id: 3, username: 'Baz'}
+    ];
+
+    http.get.and.returnValue(Observable.of(testUsers));
+
+    userService.getUsers().then(users => {
+      expect(users).toEqual(testUsers);
+      done();
+    });
+  });
+
 });
