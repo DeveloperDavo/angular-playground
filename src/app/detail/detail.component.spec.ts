7343dda1a228a8fef5a692bdecb8b188f0fe360e
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-09-08 16:17:55 +0200

    Detail component edits user's details

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -3,6 +3,7 @@ import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/t
 import {DetailComponent} from './detail.component';
 import {By} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {DebugElement} from '@angular/core';
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
@@ -25,29 +26,37 @@ describe('DetailComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should edit user\'s name', fakeAsync(() => {
-    component.user = {id: 0, name: 'Bob', username: 'Bobby'};
+  it('should edit user\'s details', fakeAsync(() => {
+    component.user = {id: 0, name: 'Bob', username: 'Bobby', email: 'bobby@gmail.com', phone: '1234'};
 
     fixture.detectChanges();
     tick();
     fixture.detectChanges();
 
-    const debugElement = fixture.debugElement.query(By.css('#input-name'));
-    debugElement.nativeElement.value = 'Tom';
+    const inputNameEl = fixture.debugElement.query(By.css('#input-name')).nativeElement;
+    updateInputField(inputNameEl, 'Tom');
+
+    const inputUsernameEl = fixture.debugElement.query(By.css('#input-username')).nativeElement;
+    updateInputField(inputUsernameEl, 'Tommy');
+
+    const inputEmailEl = fixture.debugElement.query(By.css('#input-email')).nativeElement;
+    updateInputField(inputEmailEl, 'tommy@gmail.com');
+
+    const inputPhoneEl = fixture.debugElement.query(By.css('#input-phone')).nativeElement;
+    updateInputField(inputPhoneEl, '5678');
+
+    const user = component.user;
+    expect(user.name).toBe('Tom');
+    expect(user.username).toBe('Tommy');
+    expect(user.email).toBe('tommy@gmail.com');
+    expect(user.phone).toBe('5678');
+  }));
+
+  function updateInputField(element: any, inputValue: string) {
+    element.value = inputValue;
     const event = new Event('input');
-    debugElement.nativeElement.dispatchEvent(event);
-
-    expect(component.user.name).toBe('Tom');
-  }));
-
-  it('should render username', async(() => {
-    component.user = {id: 0, username: 'Bob'};
-
-    fixture.detectChanges();
-
-    const debugElements = fixture.debugElement.queryAll(By.css('#user-details div'));
-    expect(debugElements[1].nativeElement.textContent).toContain('username: Bob');
-  }));
+    element.dispatchEvent(event);
+  }
 
   it('should render user\'s details', fakeAsync(() => {
     component.user = {id: 0, name: 'Foo Bar', username: 'foobar', email: 'foobar@gmail.com', phone: '1234'};
@@ -56,11 +65,11 @@ describe('DetailComponent', () => {
     tick();
     fixture.detectChanges();
 
-    expect(fixture.debugElement.query(By.css('#input-name')).nativeElement.value).toBe('Foo Bar');
-    const debugElements = fixture.debugElement.queryAll(By.css('#user-details div'));
-    expect(debugElements[1].nativeElement.textContent).toContain('username: foobar');
-    expect(debugElements[2].nativeElement.textContent).toContain('email: foobar@gmail.com');
-    expect(debugElements[3].nativeElement.textContent).toContain('phone: 1234');
+    const debugElement = fixture.debugElement;
+    expect(debugElement.query(By.css('#input-name')).nativeElement.value).toBe('Foo Bar');
+    expect(debugElement.query(By.css('#input-username')).nativeElement.value).toBe('foobar');
+    expect(debugElement.query(By.css('#input-email')).nativeElement.value).toBe('foobar@gmail.com');
+    expect(debugElement.query(By.css('#input-phone')).nativeElement.value).toBe('1234');
 
   }));
 });
