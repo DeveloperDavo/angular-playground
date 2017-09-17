7e53a4dcab0133f80c99be70a2841ddd5951c0eb
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-09-17 21:03:44 +0200

    Use FormBuilder in DetailComponent

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {User} from '../user';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-detail',
@@ -9,15 +9,24 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class DetailComponent {
   @Input() user: User;
-  detailForm = new FormGroup({
-    name: new FormControl(),
-    username: new FormControl(),
-    email: new FormControl(),
-    phone: new FormControl(),
-    address: new FormGroup({
-      street: new FormControl(),
-      city: new FormControl(),
-      zipcode: new FormControl()
-    })
-  });
+  detailForm: FormGroup;
+
+  constructor(private fBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.detailForm = this.fBuilder.group({
+      name: new FormControl(),
+      username: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+      address: this.fBuilder.group({
+        street: new FormControl(),
+        city: new FormControl(),
+        zipcode: new FormControl()
+      })
+    });
+  }
+
 }
