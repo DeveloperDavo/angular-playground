ff7b041ed2e16023d60684eb3505ec6788ef6285
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-09-19 21:01:21 +0200

    Spike routing: add router outlet for main/detail/temp
    and generate TempComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,30 @@ import {UserService} from './user.service';
 import {HttpClientModule} from '@angular/common/http';
 import {DetailComponent} from './detail/detail.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {TempComponent} from './temp/temp.component';
+
+const appRoutes: Routes = [
+  {path: 'main', component: MainComponent},
+  {path: 'detail', component: DetailComponent},
+  {path: 'temp', component: TempComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    DetailComponent
+    DetailComponent,
+    TempComponent
   ],
   imports: [
     ReactiveFormsModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
