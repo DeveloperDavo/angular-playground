544a27776386aa8d3285170edbe3d88dd34acc79
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-10-08 19:46:58 +0200

    Fire dropModel event in sample comp

--- src/app/sample/sample.component.spec.ts
+++ src/app/sample/sample.component.spec.ts
@@ -1,17 +1,19 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SampleComponent} from './sample.component';
-import {DragulaModule} from 'ng2-dragula';
+import {DragulaModule, DragulaService} from 'ng2-dragula';
 import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe('SampleComponent', () => {
   let component: SampleComponent;
   let fixture: ComponentFixture<SampleComponent>;
+  let dragulaService: DragulaService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [DragulaModule],
       declarations: [SampleComponent],
+      providers: [DragulaService],
       schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
@@ -20,10 +22,18 @@ describe('SampleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SampleComponent);
     component = fixture.componentInstance;
+
+    dragulaService = fixture.debugElement.injector.get(DragulaService);
+
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fire dropModel event', () => {
+    dragulaService.dropModel.emit([]);
+    expect(component.wasDropped).toBe(true);
   });
 });
